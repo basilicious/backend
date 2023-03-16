a982837b3c67f3ec10cf92ad82d9c95f3ed03002
Basil Labib <basillabib01@gmail.com>
2023-03-16 15:12:31 +0530

    Remove: CORS temporarily

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,6 @@ import CONFIG from './utils/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    // TODO: setup CORS properly
-    origin: [
-      'http://localhost:3000'
-    ],
-    credentials: true
-  })
   await app.listen(CONFIG.PORT);
 }
 bootstrap();
